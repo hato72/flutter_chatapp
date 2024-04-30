bf7f7b967825383b2594200d2f41103beaa86a87
hato72 <hatotori72@gmail.com>
2024-04-30 23:18:30 +0900

    新たなapi_handlerを追加

---
+++ lib/voice/handler/openapi_handler.dart
@@ -0,0 +1,85 @@
+import 'dart:convert';
+
+import 'package:flutter_chatapp/consts.dart';
+import 'package:http/http.dart' as http;
+
+class OpenAIService {
+  final List<Map<String, String>> messages = [];
+
+  Future<String> chatgpt(String prompt) async {
+    try {
+      final res = await http.post(
+        Uri.parse('https://api.openai.com/v1/chat/completions'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $NEXT_PUBLIC_OPENAI_API_KEY',
+        },
+        body: jsonEncode({
+          "model": "gpt-3.5-turbo",
+          "messages": [
+            {
+              'role': 'user',
+              'content':
+                  'Does this message want to generate an AI picture, image, art or anything similar? $prompt . Simply answer with a yes or no.',
+            }
+          ],
+        }),
+      );
+      //print(res.body);
+      if (res.statusCode == 200) {
+        String content =
+            jsonDecode(res.body)['choices'][0]['message']['content'];
+        content = content.trim();
+
+        switch (content) {
+          case 'Yes':
+          case 'yes':
+          case 'Yes.':
+          case 'yes.':
+          default:
+            final res = await chatGPTAPI(prompt);
+            return res;
+        }
+      }
+      return 'An internal error occurred';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future<String> chatGPTAPI(String prompt) async {
+    messages.add({
+      'role': 'user',
+      'content': prompt,
+    });
+    try {
+      final res = await http.post(
+        Uri.parse('https://api.openai.com/v1/chat/completions'),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $NEXT_PUBLIC_OPENAI_API_KEY',
+        },
+        body: jsonEncode({
+          "model": "gpt-3.5-turbo",
+          "messages": messages,
+        }),
+      );
+      print(res.body);
+
+      if (res.statusCode == 200) {
+        String content =
+            jsonDecode(res.body)['choices'][0]['message']['content'];
+        content = content.trim();
+
+        messages.add({
+          'role': 'assistant',
+          'content': content,
+        });
+        return content;
+      }
+      return 'An internal error occurred';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+}
